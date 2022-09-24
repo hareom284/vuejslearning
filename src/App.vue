3a1ce6e589c5763084877e4339a811066bef6e18
hareom284 <zawzawwin328@gmail.com>
2022-09-24 13:53:12 +0630

    update derivative and binding reactive in vuejs

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Main from "./components/Main.vue";
+// import Main1 from "./components/Main1.vue";
+</script>
+
+<template>
+  <Main />
+  <!-- <Main1 /> -->
+</template>
+
+<style scoped></style>
---
+++ src/components/Main1.vue
@@ -0,0 +1,37 @@
+/* @composite api * */
+
+<script>
+export default {
+  data() {
+    return {
+      data: 0,
+      htmlText: "<h1>Hello World</h1>",
+      isDisable: false,
+    };
+  },
+
+  methods: {
+    increase() {
+      this.data++;
+      this.isDisable = !this.isDisable;
+    },
+    decrease() {
+      this.data--;
+    },
+  },
+};
+</script>
+<template>
+  <div class="container mt-3 d-inline-block">
+    <button class="btn btn-success" @click="decrease">Decrease</button>
+    <p>{{ data }}</p>
+    <div v-html="htmlText"></div>
+    <button class="btn btn-primary" @click="increase">Increase</button>
+    <div class="row">
+      <div class="col-2">
+        <h1>Disable Button</h1>
+        <button :disabled="isDisable" class="btn btn-danger">Click</button>
+      </div>
+    </div>
+  </div>
+</template>
