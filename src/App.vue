71a7f7d6332086bbb9166fcfbc3a495245857407
hareom284 <zawzawwin328@gmail.com>
2022-09-25 22:44:49 +0630

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <script setup>
-import Main from "./components/Main.vue";
-// import Main1 from "./components/Main1.vue";
+import { ref } from "vue";
+import Hello from "./Hello.vue";
+const message = ref("hello");
 </script>
-
 <template>
-  <Main />
-  <!-- <Main1 /> -->
-</template>
+  <!-- <div>{{ lists }}</div>
+  <div class="form-group">
+    <input type="checkbox" value="hare" v-model="lists" />
+    <label for="hare">Hare</label>
+  </div>
+  <div class="form-group">
+    <input type="checkbox" value="zaw" v-model="lists" />
+    <label for="hare">Hare</label>
+  </div>
+  <div class="form-group">
+    <input type="checkbox" value="someoone" v-model="lists" />
+    <label for="hare">Hare</label>
+  </div>
+  <div class="form-group">
+    <input type="checkbox" value="yoyo" v-model="lists" />
+    <label for="hare">Hare</label>
+  </div> -->
 
-<style scoped></style>
+  <div>
+    <h1>Passing Props</h1>
+    <Hello />
+    <p>{{ message }}</p>
+  </div>
+</template>
---
+++ src/Hello.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps(["message"]);
+const 
+console.log(props);
+defineEmits(["answer"]);
+</script>
+<template>
+  <h1>Hello World</h1>
+  <p>{{ ` ${props.message}  ?` }}</p>
+  <button @click="$emit('answer')">Send</button>
+</template>
